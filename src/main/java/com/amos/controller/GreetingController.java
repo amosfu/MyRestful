8f8f3acf0d3c0c665b00dac3477e7081441336cb
fs1984 <amosfs@gmail.com>
2019-09-20 14:40:56 -0400

    initial commit

--- src/main/java/com/amos/controller/GreetingController.java
+++ src/main/java/com/amos/controller/GreetingController.java
@@ -1,14 +1,7 @@
 package com.amos.controller;
-import static org.springframework.hateoas.mvc.ControllerLinkBuilder.*;
 
-import com.amos.bean.Account;
-import org.springframework.hateoas.Link;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class GreetingController {
